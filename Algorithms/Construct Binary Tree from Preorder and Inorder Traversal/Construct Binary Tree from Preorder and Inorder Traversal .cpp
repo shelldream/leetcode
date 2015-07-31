a4f92194a359a06bc49311bbf99dd77e46032c37
unknown <huangxiaojun@DC3E4572896DEFE.internal.baidu.com>
2015-07-31 19:44:36 +0800

    add a solution to Algorithms/Binary Tree Preorder Traversal

---
+++ Algorithms/Construct Binary Tree from Preorder and Inorder Traversal/Construct Binary Tree from Preorder and Inorder Traversal .cpp
@@ -0,0 +1,63 @@
+/*
+	Construct Binary Tree from Preorder and Inorder Traversal
+	Author:Xiaojun Huang
+	Date:2015-07-31
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int j = preorder.size()-1;
+        int y = inorder.size()-1;
+        return buildFunc(preorder, 0, j, inorder, 0, y);
+    }
+    TreeNode* buildFunc(vector<int>& preorder, int i, int j, vector<int>& inorder, int x, int y)
+    {
+        if(i > j || x > y || i<0 || j<0 || x<0 || y <0 || i>=preorder.size() || j>=preorder.size() || x>=inorder.size() || y>=inorder.size())
+            return NULL;
+        int root_val = preorder[i];
+        TreeNode* root = new TreeNode(root_val);
+        int inorder_root_index = x;
+        while(inorder[inorder_root_index] != root_val)
+            inorder_root_index++; //查找根节点在后序数组中的下标
+        int left_tree_node_count = inorder_root_index-x;
+        int right_tree_node_count = y-inorder_root_index;
+        root->left = buildFunc(preorder, i+1, i+left_tree_node_count, inorder, x, x+left_tree_node_count-1);
+        root->right = buildFunc(preorder, i+left_tree_node_count+1, j, inorder, inorder_root_index+1, y);
+        return root;
+    }
+};
+
+void preorderTraversal(TreeNode* root) {//打印先序遍历的结果
+        if(root !=NULL )
+        {
+            cout<<root->val<<" ";
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+        }
+}
+
+int main()
+{
+    Solution solution;
+    int data1[] = {1,2,4,5,6,7,3,8,9,10};
+    int data2[] = {5,4,6,7,2,1,8,3,10,9};
+    int n1 = sizeof(data1)/sizeof(data1[0]);
+    int n2 = sizeof(data2)/sizeof(data2[0]);
+    vector<int> preorder(data1,data1+n1);
+    vector<int> inorder(data2, data2+n2);
+    TreeNode* root = solution.buildTree(preorder,inorder);
+    preorderTraversal(root);
+}
+
