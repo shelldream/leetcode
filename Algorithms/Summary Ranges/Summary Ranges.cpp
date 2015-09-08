190f0801e7c26f3fd8d0eabb0b8eb151499a0797
Kobe1990HXJ <Kobe1990HXJ@163.com>
2015-09-08 15:06:50 +0800

    a little fix on Algorithms/Summary Ranges

--- Algorithms/Summary Ranges/Summary Ranges.cpp
+++ Algorithms/Summary Ranges/Summary Ranges.cpp
@@ -12,12 +12,32 @@ using namespace std;
 
 class Solution {
 public:
+    string int2str(long long x)
+    {
+        if(x == 0)
+            return "0";
+        string ans = "";
+        if(x < 0)
+        {
+            ans += "-";
+            x = -x;
+        }
+        vector<char> tmp;
+        while(x)
+        {
+            tmp.push_back('0' + x%10);
+            x /= 10;
+        }
+        for(int i = tmp.size()-1; i >=0;i--)
+            ans += tmp[i];
+        return ans;
+    }
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> ans;
         if(nums.size() == 0)
             return ans;
-        int start = nums[0];
-        int end = nums[0];
+        long long start = nums[0];
+        long long end = nums[0];
         for(int i = 0;i < nums.size();i++)
         {
             if(i+1 < nums.size() && (nums[i+1]-nums[i] == 1))
@@ -27,13 +47,13 @@ public:
             else
             {
                 string tmp = "";
-                tmp += ('0' + start);
+                tmp += int2str(start);
                 if(start == end)
                     ans.push_back(tmp);
                 else
                 {
                     tmp += "->";
-                    tmp += ('0' + end);
+                    tmp += int2str(end);
                     ans.push_back(tmp);
                 }
                 if(i+1 < nums.size())
@@ -50,8 +70,8 @@ public:
 
 int main()
 {
-    //int data[] = {0,1,2,4,5,7};
-    int data[] = {0,3};
+    int data[] = {0,1,2,4,5,7};
+    //int data[] = {-2147483648,-2147483647,2147483647};
     int n = sizeof(data)/sizeof(data[0]);
     vector<int> nums(data,data+n);
     Solution test;
