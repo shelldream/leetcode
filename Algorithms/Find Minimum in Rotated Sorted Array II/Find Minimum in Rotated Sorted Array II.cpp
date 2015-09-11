a5b24db69e9dbd22a4821885605be7eac46a6095
Kobe1990HXJ <Kobe1990HXJ@163.com>
2015-09-11 12:26:19 +0800

    add a solution to Algorithms/Find Minimum in Rotated Sorted Array II

--- Algorithms/Find Minimum in Rotated Sorted Array II/Find Minimum in Rotated Sorted Array II.cpp
+++ Algorithms/Find Minimum in Rotated Sorted Array II/Find Minimum in Rotated Sorted Array II.cpp
@@ -1,7 +1,7 @@
 /**
-	Find Minimum in Rotated Sorted Array
+	Find Minimum in Rotated Sorted Array II
 	Author:Xiaojun Huang
-	Date:2015-08-22
+	Date:2015-09-11
 
 */
 
@@ -11,6 +11,20 @@ using namespace std;
 
 class Solution {
 public:
+    int findMinInorder(vector<int>& nums, int low, int high)
+    {
+        int min_val = nums[low];
+        while(low < high)
+            if(min_val <= nums[low+1])
+                low++;
+            else
+            {
+                min_val = nums[low+1];
+                break;
+            }
+
+        return min_val;
+    }
     int findMin(vector<int>& nums) {
         int low = 0;
         int high = nums.size()-1;
@@ -25,6 +39,8 @@ public:
                 high = mid;
             else if(nums[mid] > nums[low] && nums[mid] < nums[high])
                 high = mid;
+            if(nums[mid] == nums[low] || nums[mid] == nums[high])
+                return findMinInorder(nums, low,high);
             if(high-low == 1)
                 return min(nums[low],nums[high]);
         }
@@ -35,7 +51,8 @@ int main()
 {
     //int data[] = {4,5,6,7,8,-1,1,2,3};
     //int data[] = {-1,0,2,3};
-    int data[] = {1};
+    //int data[] = {1,1,1,1,1,2,0,1};
+    int data[] = {1,1,1,3};
     int n = sizeof(data)/sizeof(data[0]);
     vector<int> nums(data,data+n);
     Solution solution;
